1311f1d43d2b7edfe04ea08fb4ca698114869244
youbin-shin <youbin5575@gmail.com>
2020-06-11 19:10:33 +0900

    modeling commit

--- movies/admin.py
+++ movies/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Genre, Movie, Comment, Community
 
-# Register your models here.
+
+admin.site.register(Genre)
+admin.site.register(Movie)
+admin.site.register(Community)
+admin.site.register(Comment)
--- movies/models.py
+++ movies/models.py
@@ -1,3 +1,40 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+
+class Genre(models.Model):
+    name = models.CharField(max_length=100)
+
+
+class Movie(models.Model):
+    title = models.CharField(max_length=100)
+    original_title = models.CharField(max_length=100)
+    release_date = models.DateField()
+    popularity = models.FloatField()
+    vote_count = models.IntegerField()
+    vote_average = models.FloatField()
+    adult = models.BooleanField()
+    overview = models.TextField()
+    original_language = models.CharField(max_length=100)
+    poster_path = models.CharField(max_length=200)
+    backdrop_path = models.CharField(max_length=200)
+    genres = models.ManyToManyField(Genre, related_name='movies')
+    like_users = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name='like_movies')
+
+
+class Community(models.Model):
+    title = models.CharField(max_length=140)
+    content = models.TextField()
+    rank = models.IntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+
+
+class Comment(models.Model):
+    community = models.ForeignKey(Community, on_delete=models.CASCADE)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+
