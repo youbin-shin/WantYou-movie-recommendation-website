12a2035bb4f549d6b52ff352d0704d79134a4b8e
조완석 <wansuk771@gmail.com>
2020-06-15 13:34:46 +0900

    font, community, profile design

--- movies/urls.py
+++ movies/urls.py
@@ -6,7 +6,6 @@ app_name = 'movies'
 
 urlpatterns = [
     path('', views.home, name='home'),
-    
     path('index/', views.index, name='index'), # homepage
     path('<int:movie_pk>/detail/', views.detail, name='detail'),
     path('<int:movie_pk>/like/', views.like, name='like'),
